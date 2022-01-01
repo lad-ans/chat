f6df1bd408bcf60329a37f26d40a73e49bc2d3fc
Ladino Anselmo <56450813+layndev@users.noreply.github.com>
2022-01-01 10:14:46 +0200

    add heroku url

--- lib/app/global/environment.dart
+++ lib/app/global/environment.dart
@@ -3,13 +3,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 class Environment {
-  static String apiURL = Platform.isAndroid 
-    ? 'http://10.0.2.2:3000/api'
-    : 'http://192.168.1.101:3000/api'; 
+  // static String apiURL = Platform.isAndroid 
+  //   ? 'http://10.0.2.2:3000/api'
+  //   : 'http://192.168.1.101:3000/api'; 
 
-  static String socketURL = Platform.isAndroid 
-    ? 'http://10.0.2.2:3000'
-    : 'http://192.168.1.101:3000'; 
+  // static String socketURL = Platform.isAndroid 
+  //   ? 'http://10.0.2.2:3000'
+  //   : 'http://192.168.1.101:3000'; 
+
+  static String apiURL = 'https://flutter-chat-backend-server.herokuapp.com/api';
+  static String socketURL = 'https://flutter-chat-backend-server.herokuapp.com';
 
   
   static final  msngrKey = GlobalKey<ScaffoldMessengerState>();
